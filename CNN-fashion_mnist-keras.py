b4c46324e86ff99b7e75c902949bb6637cc81ea9
Raylla-rs <66892096+Raylla-rs@users.noreply.github.com>
2020-10-27 16:13:42 -0300

    Update CNN-fashion_mnist-keras.py

--- CNN-fashion_mnist-keras.py
+++ CNN-fashion_mnist-keras.py
@@ -13,6 +13,7 @@ from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 #para o treinamento:
 import keras
+from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
